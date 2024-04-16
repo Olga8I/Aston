b0dd31df1dfd5c6754f66246871ee77e443e49c8
Olga Filonchik <olga.filonchik@gmail.com>
2024-04-16 11:30:55 +0300

    Java data from lesson six

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int calculateFactorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * calculateFactorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int number = 0;
+        System.out.println("Факториал числа " + number + " число " + calculateFactorial(number));
+    }
+}
