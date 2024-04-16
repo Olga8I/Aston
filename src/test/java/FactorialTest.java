47c1c9107beb326ac489b8bebd83302a8cecef1d
Olga Filonchik <olga.filonchik@gmail.com>
2024-04-16 22:54:06 +0300

    Java data from lesson six

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -6,10 +6,15 @@ public class FactorialTest {
     @Test
     void testFactorialOfZero() {
         assertEquals(1, Main.calculateFactorial(0));
-        assertEquals(1, Main.calculateFactorial(1));
-        assertEquals(2, Main.calculateFactorial(2));
-        assertEquals(6, Main.calculateFactorial(3));
-        assertEquals(24, Main.calculateFactorial(4));
+    }
+
+    @Test
+    void testFactorialOfFive() {
         assertEquals(120, Main.calculateFactorial(5));
+    }
+
+    @Test
+    void testFactorialOfTen() {
+        assertEquals(3628800, Main.calculateFactorial(10));
     }
 }
