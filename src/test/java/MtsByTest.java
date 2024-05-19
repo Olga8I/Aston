24d153501ae912bd38f5bb1949e0f1f9d1d8abc6
Olga Filonchik <olga.filonchik@gmail.com>
2024-05-19 19:52:00 +0300

    Java data from lesson ten

---
+++ src/test/java/MtsByTest.java
@@ -0,0 +1,75 @@
+import io.qameta.allure.junit5.AllureJunit5;
+import org.junit.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+@ExtendWith(AllureJunit5.class)
+public class MtsByTest {
+    private static WebDriver driver;
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @Before
+    public void openHomePage() {
+        driver.get("https://www.mts.by/");
+    }
+
+    @Test
+    public void testServices() {
+        String[] services = {"Услуги связи", "Домашний интернет", "Рассрочка", "Задолженность"};
+        for (String pay : services) {
+            WebElement serviceInput = driver.findElement(By.id(pay));
+            Assert.assertNotNull("Надпись в незаполненном поле " + pay + " не найдена", serviceInput.getAttribute("placeholder"));
+        }
+    }
+
+    @Test
+    public void testPhoneDisplay() {
+        WebElement phoneDisplay = driver.findElement(By.id("connection-phone"));
+        Assert.assertEquals("Некорректное отображение номера телефона", "Номер телефона: 297777777", phoneDisplay.getText());
+    }
+
+    @Test
+    public void testSumDisplay() {
+        WebElement sumDisplay = driver.findElement(By.id("connection-sum"));
+        Assert.assertEquals("Некорректное отображение суммы", "Сумма: 1000 руб.", sumDisplay.getText());
+    }
+
+    @Test
+    public void testCardDetails() {
+        WebElement cardDetails = driver.findElement(By.xpath("//div[@class='app-wrapper__content-container app-wrapper__content-container_fulls']"));
+        Assert.assertNotNull("Надпись в незаполненном поле для ввода реквизитов карты не найдена", cardDetails.getAttribute("placeholder"));
+    }
+
+    @Test
+    public void testPaymentLogos() {
+        WebElement paymentLogos = driver.findElement(By.xpath("//div[@class='pay__partners']"));
+        List<WebElement> logos = paymentLogos.findElements(By.tagName("img"));
+        Assert.assertTrue("Логотипы платежных систем не найдены", logos.size() > 1);
+        for (WebElement logo : logos) {
+            Assert.assertTrue("Логотип платежной системы не отображается", logo.isDisplayed());
+        }
+    }
+
+    @After
+    public void closeHomePage() {
+        driver.close();
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        driver.quit();
+    }
+}
+
+
+
